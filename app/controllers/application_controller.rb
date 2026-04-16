f9226e46b28c641ad3cfabea54436a5a6c129e39
Pablo Lucas <lucasrn03@gmail.com>
2026-04-16 20:25:38 -0300

    feat: implementa tela de registro com layout para o devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  prepend_view_path Rails.root.join("app/views/components")
+
+  layout :layout_by_resource
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def layout_by_resource
+    devise_controller? ? "devise" : "application"
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
